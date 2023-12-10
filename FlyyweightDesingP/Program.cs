772a2e38c7941de675298cfe88d050ada49bb8d3
= <=Turqay@mail.ru>
2023-12-10 13:55:16 +0400

    Add project files.

---
+++ FlyyweightDesingP/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flyweight
+{
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int extrinsicstate = 22;
+
+            FlyweightFactory factory = new FlyweightFactory();
+
+
+            Flyweight fx = factory.GetFlyweight("X");
+            fx.Operation(--extrinsicstate);
+
+            Flyweight fy = factory.GetFlyweight("Y");
+            fy.Operation(--extrinsicstate);
+
+            Flyweight fz = factory.GetFlyweight("Z");
+            fz.Operation(--extrinsicstate);
+
+            UnsharedConcreteFlyweight fu = new
+                UnsharedConcreteFlyweight();
+
+            fu.Operation(--extrinsicstate);
+
+
+            Console.ReadKey(); //Codu sAXLAMA
+        }
+    }
+
+
+    public class FlyweightFactory
+    {
+        private Dictionary<string, Flyweight> flyweights { get; set; } = new Dictionary<string, Flyweight>();
+
+
+        public FlyweightFactory()
+        {
+            flyweights.Add("X", new ConcreteFlyweight());
+            flyweights.Add("Y", new ConcreteFlyweight());
+            flyweights.Add("Z", new ConcreteFlyweight());
+        }
+
+        public Flyweight GetFlyweight(string key)
+        {
+            return ((Flyweight)flyweights[key]);
+        }
+    }
+
+ 
+
+    public abstract class Flyweight
+    {
+        public abstract void Operation(int extrinsicstate);
+    }
+
+
+
+    public class ConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("ConcreteFlyweight: " + extrinsicstate);
+        }
+    }
+
+ 
+
+    public class UnsharedConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("UnsharedConcreteFlyweight: " + extrinsicstate);
+        }
+    }
+}
